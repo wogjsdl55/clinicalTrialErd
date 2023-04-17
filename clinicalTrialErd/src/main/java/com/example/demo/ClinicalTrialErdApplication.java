c39b454df7d34c31ecb0a4d8953c74bfaed27be9
1-23-049 <1-23-049@DCISL973.dreamcis.com>
2023-04-17 09:38:01 +0900

    토이프로젝트 셋팅 진행중 입니다.

---
+++ clinicalTrialErd/src/main/java/com/example/demo/ClinicalTrialErdApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicalTrialErdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicalTrialErdApplication.class, args);
+	}
+
+}
