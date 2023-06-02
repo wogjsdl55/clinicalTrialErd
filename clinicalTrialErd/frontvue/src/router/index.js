54c56ae4db3b9db9234380f44f749a1753a8eaa1
1-23-049 <1-23-049@DCISL973.dreamcis.com>
2023-06-02 13:36:40 +0900

    수정 진행중 입니다.

--- clinicalTrialErd/frontvue/src/router/index.js
+++ clinicalTrialErd/frontvue/src/router/index.js
@@ -17,7 +17,6 @@ import Administration from '../pages/Administration.vue';
 
 Vue.use(Router);
 export default new Router({
-  mode: 'history',
   routes: [
     {
       path: '/dashboard',
--- clinicalTrialErd/src/main/java/clinical/springConfig.java
+++ clinicalTrialErd/src/main/java/clinical/springConfig.java
@@ -1,16 +1,37 @@
 package clinical;
 
+import io.jsonwebtoken.io.IOException;
+import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
-//@Configuration
+@Configuration
 public class springConfig implements WebMvcConfigurer {
 	//@Override
 //	public void addCorsMappings(CorsRegistry registry) {
-//		//vue와 spring cros 이슈 해결
+		//vue와 spring cros 이슈 해결
 //		registry.addMapping("/**")
 //		.allowedMethods("GET", "POST")
 //	    .allowedOrigins("http://localhost:8080", "http://localhost:8081");
-//	}
+//    }
+
+
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/**/*")
+//                .addResourceLocations("classpath:/static/")
+//                .resourceChain(true)
+//                .addResolver(new PathResourceResolver() {
+//            @Override
+//            protected Resource getResource(String resourcePath, Resource location) throw IOException {
+//                Resource requestedResource = location.create
+//            }
+//
+//        });
+//        WebMvcConfigurer.super.addResourceHandlers(registry);
+//    }
+
 }
