2fec0e8e3737399cb6434f1575eb3abbd6b76c28
1-23-049 <1-23-049@DCISL973.dreamcis.com>
2023-06-02 14:18:01 +0900

    .

--- clinicalTrialErd/frontvue/src/router/index.js
+++ clinicalTrialErd/frontvue/src/router/index.js
@@ -17,7 +17,7 @@ import Administration from '../pages/Administration.vue';
 
 Vue.use(Router);
 export default new Router({
-  //mode: 'history',
+  mode: 'history',
   routes: [
     {
       path: '/dashboard',
